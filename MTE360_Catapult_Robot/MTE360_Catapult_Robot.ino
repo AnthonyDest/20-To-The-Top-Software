bd7560d16b3c75bee4b614325abc2c2f20984c5a
Anthony Dest <37128846+AnthonyDest@users.noreply.github.com>
2023-02-11 18:26:08 -0500

    SD Card Library
    Added existing SD card library and communication library, will need to test this and default SD card code

--- MTE360_Catapult_Robot/MTE360_Catapult_Robot.ino
+++ MTE360_Catapult_Robot/MTE360_Catapult_Robot.ino
@@ -1,4 +1,6 @@
 #include "TB6612_Motor.h"
+#include <SPI.h>
+#include <SD.h>
 // Limited functionality, only 4/6 combos due to pins
 // Pins for all inputs (NOT CONFIGURED YET)
 #define DIR1_M1 10// LEFT
@@ -24,17 +26,42 @@
 const int DirectionInvertLeft = 1;
 const int DirectionInvertRight = -1;
 
+// change this to match the SD shield or module;
+const int chipSelect = 10;
+File dataLog;
+
 // Initializing
 Motor leftMotor = Motor(DIR1_M1, PWM_M1, DirectionInvertLeft);
 Motor rightMotor = Motor(DIR1_M2, PWM_M2, DirectionInvertRight);
 Encoder leftEncoder = Encoder(ENCA_M1,ENCB_M1); // digital pin
 Encoder rightEncoder = Encoder(ENCA_M2,ENCB_M2); // analog pin, dont think digital read works on nanos - will need testing
-//add encoder here and put in robot object
+
+// Will add sd card file as parameter later so we can log anytime a command was run in all other functions
 
 Robot robot = Robot(leftMotor, rightMotor, leftEncoder, rightEncoder);
 
 void setup() {
   // need to clock down based on Lana's undervolting (change 9600)
+
+    if (!SD.begin()) { // can put in slave pin as parameter
+    Serial.println("SD CARD FAILED, OR NOT PRESENT!");
+  }
+  Serial.println("SD CARD initialization sucessful");
+
+  // have each save be unique based on current time so we can track multipule results at a time?
+ dataLog = SD.open("dataLog.txt", FILE_WRITE);
+
+// This code can be moved to loop, dont want to put now and continously write to file until we test a bit more
+// if the file opened okay, write to it:
+ if (dataLog) {
+    dataLog.println("testing 1, 2, 3.");
+    // close the file:
+    dataLog.close();
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening file");
+  }
+
   Serial.begin(9600);
 }
 
