825a4d742516393273dfdae0e8a61f63cf3ebcf3
Anthony Dest <37128846+AnthonyDest@users.noreply.github.com>
2023-02-16 19:38:54 -0500

    Testing Motors

--- MTE360_Catapult_Robot/MTE360_Catapult_Robot.ino
+++ MTE360_Catapult_Robot/MTE360_Catapult_Robot.ino
@@ -1,106 +1,90 @@
 #include "TB6612_Motor.h"
 #include <SPI.h>
-#include <SD.h>
-// Limited functionality, only 4/6 combos due to pins
-// Pins for all inputs (NOT CONFIGURED YET)
-#define DIR1_M1 10// LEFT
-#define DIR1_M2 11 // RIGHT
+// #include <SD.h>
 
-#define PWM_M1 9// LEFT
-#define PWM_M2 8// RIGHT
+// // Limited functionality, only 4/6 combos due to pins
+// // Pins for all inputs (NOT CONFIGURED YET)
+#define DIR1_M1 7  // LEFT 10
+#define DIR1_M2 8  // RIGHT 11
+#define PWM_M1 6   // LEFT 9
+#define PWM_M2 9   // RIGHT 12
 
-// Pins for encoder
-// Digital pins
-#define ENCA_M1 6
-#define ENCB_M1 5
-// Analog pins to be converted to digital (doesn't auto convert on nano)
-#define ENCA_M2 21
-#define ENCB_M2 22
+// // Pins for encoder
+// // Digital pins
+//left currently
+#define ENCA_M1 3
+#define ENCB_M1 2
+// // Analog pins to be converted to digital (might not auto convert on nano), try A0 to A5
+//Right Currently
+#define ENCA_M2 A2
+#define ENCB_M2 A3
 
-// these constants are used to allow you to make your motor configuration
-// line up with function names like forward.  Value can be 1 or -1
-
-//used to flip direction so forward function spins each wheel the correct direction (based on wiring and orientation)
-// const bool DirectionInvertLeft = TRUE;
-// const bool DirectionInvertRight = TRUE;
+// //used to flip direction so forward function spins each wheel the correct direction (based on wiring and orientation)
 const int DirectionInvertLeft = 1;
-const int DirectionInvertRight = -1;
+const int DirectionInvertRight = 1;
 
-// change this to match the SD shield or module;
-const int chipSelect = 10;
-File dataLog;
+// // change this to match the SD shield or module;
+// const int chipSelect = 10;
+// File dataLog;
 
-// Initializing
+// // Initializing
 Motor leftMotor = Motor(DIR1_M1, PWM_M1, DirectionInvertLeft);
 Motor rightMotor = Motor(DIR1_M2, PWM_M2, DirectionInvertRight);
-Encoder leftEncoder = Encoder(ENCA_M1,ENCB_M1); // digital pin
-Encoder rightEncoder = Encoder(ENCA_M2,ENCB_M2); // analog pin, dont think digital read works on nanos - will need testing
+Encoder leftEncoder = Encoder(ENCA_M1, ENCB_M1, DirectionInvertLeft);    // digital pin
+Encoder rightEncoder = Encoder(ENCA_M2, ENCB_M2, DirectionInvertRight);  // analog pin, dont think digital read works on nanos - will need testing
 
-// Will add sd card file as parameter later so we can log anytime a command was run in all other functions
+// Robot robot = Motor(leftMotor, rightMotor, leftEncoder, rightEncoder);
 
-Robot robot = Robot(leftMotor, rightMotor, leftEncoder, rightEncoder);
+// // Will add sd card file as parameter later so we can log anytime a command was run in all other functions
 
 void setup() {
-  // need to clock down based on Lana's undervolting (change 9600)
+  //     if (!SD.begin()) { // can put in slave pin as parameter
+  //     Serial.println("SD CARD FAILED, OR NOT PRESENT!");
+  //   }
+  //   Serial.println("SD CARD initialization sucessful");
 
-    if (!SD.begin()) { // can put in slave pin as parameter
-    Serial.println("SD CARD FAILED, OR NOT PRESENT!");
-  }
-  Serial.println("SD CARD initialization sucessful");
+  //   // have each save be unique based on current time so we can track multipule results at a time?
+  //  dataLog = SD.open("dataLog.txt", FILE_WRITE);
 
-  // have each save be unique based on current time so we can track multipule results at a time?
- dataLog = SD.open("dataLog.txt", FILE_WRITE);
-
-// This code can be moved to loop, dont want to put now and continously write to file until we test a bit more
-// if the file opened okay, write to it:
- if (dataLog) {
-    dataLog.println("testing 1, 2, 3.");
-    // close the file:
-    dataLog.close();
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening file");
-  }
+  // This code can be moved to loop, dont want to put now and continously write to file until we test a bit more
+  // // if the file opened okay, write to it:
+  //  if (dataLog) {
+  //     dataLog.println("testing 1, 2, 3.");
+  //     // close the file:
+  //     dataLog.close();
+  //   } else {
+  //     // if the file didn't open, print an error:
+  //     Serial.println("error opening file");
+  //   }
 
   Serial.begin(9600);
+  // leftMotor.speedUp(50, 0, 1);
+  // rightMotor.fwd(100);
+
+  // leftEncoder.resetCounter();
+  // rightEncoder.resetCounter();
+  // Serial.println("RESET");
+  // Serial.println("aa" + (unsigned long)&leftMotor);
+
+
+  // Serial.println("LEFT IN COIDE: " + (unsigned long)&leftMotor);
+  //  Serial.println("LEFT IN BOT: " + (unsigned long)&robot.leftMotor);
 }
 
 
 void loop() {
-  robot.forwardDrive(100);
-  delay(3000);
 
-  robot.brake();
-  delay(3000);
+  // leftEncoder.EncScanActive();
+  // rightEncoder.EncScanActive();
 
-  robot.backwardDrive(100);
-  delay(3000);
+  //  leftMotor.rev(200);
+  // rightMotor.rev(200);
 
-  robot.brake();
-  delay(3000);
+  // delay(2000);
 
-  robot.rightTurnStationary(100);
-  delay(3000);
+  //  leftMotor.rev(0);
+  //  rightMotor.rev(0);
 
-  robot.brake();
-  delay(3000);
-
-  robot.leftTurnStationary(100);
-  delay(3000);
-
-  robot.brake();
-  delay(3000);
-
-  robot.forwardDriveDistance(200,60);
-  delay(3000);
-
-  robot.brake();
-  delay(3000);
-
-  robot.reverseDriveDistance(200,60);
-  delay(3000);
-
-  robot.brake();
-  delay(3000);
+  //  delay(50000);
 
 }
--- MTE360_Catapult_Robot/TB6612_Motor.cpp
+++ MTE360_Catapult_Robot/TB6612_Motor.cpp
@@ -1,44 +1,80 @@
+#include "HardwareSerial.h"
 #include "TB6612_Motor.h"
 
 #include <Arduino.h>
+#include "Rotary.h"
 
 Motor::Motor() {}
 
-Motor::Motor(int DIRpin, int PWMpin, int DirectionInvert) {
-  DIR = DIRpin;
-  PWM = PWMpin;
+Motor::Motor(int _DIR, int _PWM, int _DirectionInvert) {
+  DIR = _DIR;
+  PWM = _PWM;
   // Standby = STBYpin;
-  this->DirectionInvert = DirectionInvert;
+  DirectionInvert = _DirectionInvert;
 
   pinMode(DIR, OUTPUT);
   pinMode(PWM, OUTPUT);
-
+  Serial.begin(9600);
   // pinMode(Standby, OUTPUT);
 }
 
-void Motor::speedUp(int desiredSpeed, int prevSpeed, int direction) {
-  for (curSpeed = prevSpeed; curSpeed <= desiredSpeed; curSpeed += SPEEDINCREMENT) {
+//may want to remove while if the time is too long
+void Motor::speedUp(int desiredSpeed, int curSpeed, int direction) {
+
+  while (curSpeed < desiredSpeed) {
 
     curSpeed += SPEEDINCREMENT;
-    if (curSpeed > desiredSpeed) curSpeed = desiredSpeed;
 
-    if (direction == FORWARD_DIR) fwd(desiredSpeed);
-    else rev(desiredSpeed);
+    // redundant input checks
+    if (curSpeed > desiredSpeed) {
+      curSpeed = desiredSpeed;
+    }
+
+    if (curSpeed < MINSPEED ) {
+        curSpeed = MINSPEED;
+      }
+
+    if (curSpeed > MAXSPEED) {
+      curSpeed = MAXSPEED;
+    }
+
+    if (direction == FORWARD_DIR) {
+      fwd(curSpeed);
+    } else {
+      rev(curSpeed);
+    }
+
+    delay(SPEEDDELAY);
   }
 }
+//may want to remove while if the time is too long
+void Motor::slowDown(int desiredSpeed, int curSpeed, int direction) {
 
-void Motor::slowDown(int desiredSpeed, int prevSpeed, int direction) {
-  for (curSpeed = prevSpeed; curSpeed >= desiredSpeed; curSpeed -= SPEEDINCREMENT) {
+  while (curSpeed > desiredSpeed) {
 
     curSpeed -= SPEEDINCREMENT;
-    if (curSpeed < desiredSpeed) curSpeed = desiredSpeed;
 
-    if (direction == FORWARD_DIR) fwd(desiredSpeed);
-    else rev(desiredSpeed);
+   // redundant input checks
+    if (curSpeed < desiredSpeed ){
+        curSpeed = desiredSpeed;
+      }
+
+    if (curSpeed < MINSPEED ) {
+        curSpeed = 0;
+      }
+
+    if (direction == FORWARD_DIR) {
+      fwd(curSpeed);
+    } else {
+      rev(curSpeed);
+    }
+
+    delay(SPEEDDELAY);
+
   }
 }
 
-//Determines which way to spin motor and how to scale speed
+//Determines which way to spin motor and how to scale speed 
 void Motor::drive(int desiredSpeed, int direction) {
 
   direction = direction * DirectionInvert;
@@ -51,25 +87,16 @@ void Motor::drive(int desiredSpeed, int direction) {
 
     speedUp(desiredSpeed, prevSpeed, direction);
   }
-
-  // if (direction == forward) fwd(desiredSpeed);
-  // else rev(-desiredSpeed);
 }
 
-// //Drive for a set duration
-// void Motor::drive(int speed, int duration) {
-//   drive(speed);
-//   delay(duration);
-// }
-
-int Motor::getMotorDir(){
+int Motor::getMotorDir() {
   return curDirState;
 }
 
 
 //Go Forward at entered speed
 void Motor::fwd(int speed) {
-curDirState = FORWARD_DIR;
+  curDirState = FORWARD_DIR;
   digitalWrite(DIR, HIGH);  // MAY BE LOW BASED ON ELEC NOT
   analogWrite(PWM, speed);
 }
@@ -100,141 +127,157 @@ void Motor::brake() {
 
 Encoder::Encoder() {}
 
-Encoder::Encoder(int ENCA_Pin, int ENCB_Pin) {
-  ENC_A = ENCA_Pin;
-  ENC_B = ENCB_Pin;
+Encoder::Encoder(int _ENC_A, int _ENC_B, int DirectionInvertP) {
+  // ENC_A = ENCA_Pin;
+  // ENC_B = ENCB_Pin;
+DirectionInvert = DirectionInvertP;
 
-  pinMode(ENC_A, INPUT);
-  pinMode(ENC_B, INPUT);
+  rotary = Rotary(_ENC_A, _ENC_B); //else just pass by reference
 
-  aPrevState = digitalRead(ENC_A);
-  
+
+  // pinMode(ENC_A, INPUT);
+  // pinMode(ENC_B, INPUT);
+
+  // aPrevState = digitalRead(ENC_A);
 }
 
-void Encoder::EncScanActive() {
+void Encoder::EncScanActive(){
 
-  // If the previous and the current state of the outputA are different, that means a Pulse (swap direction) has occured
-  aCurrentState = digitalRead(ENC_A);  // Reads the "current" state of the outputA
-  if (aCurrentState != aPrevState) {
-    // If the outputB state is different to the outputA state, that means the encoder is rotating clockwise
-    if (digitalRead(ENC_B) != aCurrentState) {
-      stepCounter++;
-    } else {
-      stepCounter--;
-    }
+ rotaryDirection = rotary.process();
+
+ //needs direction invert
+  if (rotaryDirection == DIR_CW) {
+    stepCounter++;
+    Serial.println(stepCounter);
+  } else if (rotaryDirection == DIR_CCW) {
+    stepCounter--;
+    Serial.println(stepCounter);
   }
-  aPrevState = aCurrentState;
+
 }
 
-void Encoder::resetCounter(){
+// void Encoder::EncScanActive() {
+
+//   // If the previous and the current state of the outputA are different, that means a Pulse (swap direction) has occured
+//   aCurrentState = digitalRead(ENC_A);  // Reads the "current" state of the outputA
+//   if (aCurrentState != aPrevState) {
+//     // If the outputB state is different to the outputA state, that means the encoder is rotating clockwise
+//     if (digitalRead(ENC_B) != aCurrentState) {
+//       stepCounter++;
+//       // Serial.println("Forward");
+//     } else {
+//       stepCounter--;
+//       // Serial.println("Backward");
+//     }
+//     //  Serial.println(stepCounter);
+//   }
+//   aPrevState = aCurrentState;
+// }
+
+void Encoder::resetCounter() {
   stepCounter = 0;
 }
 
-float Encoder::getDistanceCM(){
+float Encoder::getDistanceCM() {
 
-  return (stepCounter/stepsPerWheelRotation)*wheelDiamMM*3.14159/10; // replace pi with math library
-
+// Serial.println(float(stepCounter) / float(stepsPerWheelRotation));
+  // return ((stepCounter / float(stepsPerWheelRotation) )* float(wheelDiamMM) * float(3.14159 / 10));  // replace pi with math library
+  return (float(stepCounter) / float(stepsPerWheelRotation));
 }
-void Encoder::getMotorDirection(){} // can use encoder to sample motor and verify rotation
+void Encoder::getMotorDirection() {}  // can use encoder to sample motor and verify rotation
 
 
 /*****************************************************************************/
 
-Robot::Robot(Motor leftMotorParam, Motor rightMotorParam, Encoder leftEncoderP, Encoder rightEncoderP) {
-  Motor leftMotor = leftMotorParam;
-  Motor rightMotor = rightMotorParam;
-  Encoder leftEncoder = leftEncoderP;
-  Encoder rightEncoder = rightEncoderP;
+Robot::Robot(){};
 
+// Robot::Robot(Motor& testMotorP){
+
+//   // testMotor -> testMotorP;
+//   // Serial.println("bb" + (unsigned long)&testMotorP);
+// }
+
+
+Robot::Robot(Motor &_leftMotor, Motor &_rightMotor, Encoder &_leftEncoder, Encoder &_rightEncoder) {
+  leftMotor = &_leftMotor;
+  rightMotor = &_rightMotor;
+  leftEncoder = &_leftEncoder;
+  rightEncoder = &_rightEncoder;
 }
 
 //Drive forward
 void Robot::forwardDrive(int speed) {
   speed = abs(speed);
-  leftMotor.drive(speed, FORWARD_DIR);
-  rightMotor.drive(speed, FORWARD_DIR);
+  
+  // Serial.println("LEFT IN FXN: " + (unsigned long)&leftMotor);
+  leftMotor->drive(speed, FORWARD_DIR);
+  rightMotor->drive(speed, FORWARD_DIR);
 }
 
 
 //Drive backward
 void Robot::backwardDrive(int speed) {
   speed = abs(speed);
-  leftMotor.drive(speed, BACKWARD_DIR);
-  rightMotor.drive(speed, BACKWARD_DIR);
+  leftMotor->drive(speed, BACKWARD_DIR);
+  rightMotor->drive(speed, BACKWARD_DIR);
 }
 
 
 //Turn left
 void Robot::leftTurnStationary(int speed) {
   speed = abs(speed) / 2;
-  leftMotor.drive(speed, BACKWARD_DIR);
-  rightMotor.drive(speed, FORWARD_DIR);
+  leftMotor->drive(speed, BACKWARD_DIR);
+  rightMotor->drive(speed, FORWARD_DIR);
 }
-
-// //Turn left, default speed
-// void Robot::leftTurn() {
-//   speed = abs(DEFAULTSPEED) / 2;
-//   leftMotor.drive(-speed);
-//   rightMotor.drive(speed);
-// }
-
 
 //Turn right
 void Robot::rightTurnStationary(int speed) {
   speed = abs(speed) / 2;
-  leftMotor.drive(speed, FORWARD_DIR);
-  rightMotor.drive(speed, BACKWARD_DIR);
+  leftMotor->drive(speed, FORWARD_DIR);
+  rightMotor->drive(speed, BACKWARD_DIR);
 }
-
-// //Turn right, default speed
-// void Robot::rightTurn() {
-//   speed = abs(speed) / 2;
-//   leftMotor.drive(speed);
-//   rightMotor.drive(-speed);
-// }
 
 
 //Motor braking (hard stop)
 void Robot::brake() {
 
   // slow down (need to check current direction of motors)
-  leftMotor.drive(0, leftMotor.getMotorDir());
-  rightMotor.drive(0, rightMotor.getMotorDir());
+  leftMotor->drive(0, leftMotor->getMotorDir());
+  rightMotor->drive(0, rightMotor->getMotorDir());
 
   // stop
-  leftMotor.brake();
-  rightMotor.brake();
+  leftMotor->brake();
+  rightMotor->brake();
 }
 
 void Robot::forwardDriveDistance(int speed, float distanceCM) {
-initialDistance = average(leftEncoder.getDistanceCM(), rightEncoder.getDistanceCM()); // do we get average and compare end result avg, or do we just monitor each independently
-// eg: change to leftInital, rightInital. Distance travelled = average(DeltaLeft, DeltaRight) ?
-forwardDrive(speed);
+  initialDistance = average(leftEncoder->getDistanceCM(), rightEncoder->getDistanceCM());  // do we get average and compare end result avg, or do we just monitor each independently
+  // eg: change to leftInital, rightInital. Distance travelled = average(DeltaLeft, DeltaRight) ?
+  forwardDrive(speed);
 
-while(distanceCM >= currentDistanceTravelled){
-currentDistanceTravelled = average(leftEncoder.getDistanceCM(), rightEncoder.getDistanceCM()) - initialDistance;
-}
-currentDistanceTravelled = 0;
-brake(); // will stop currently for testing, but this can be removed later to optimize
+  while (distanceCM >= currentDistanceTravelled) {
+    currentDistanceTravelled = average(leftEncoder->getDistanceCM(), rightEncoder->getDistanceCM()) - initialDistance;
+  }
+  currentDistanceTravelled = 0;
+  brake();  // will stop currently for testing, but this can be removed later to optimize
 }
 
 void Robot::reverseDriveDistance(int speed, float distanceCM) {
-initialDistance = average(leftEncoder.getDistanceCM(), rightEncoder.getDistanceCM()); // do we get average and compare end result avg, or do we just monitor each independently
-// eg: change to leftInital, rightInital. Distance travelled = average(DeltaLeft, DeltaRight) ?
-backwardDrive(speed);
+  initialDistance = average(leftEncoder->getDistanceCM(), rightEncoder->getDistanceCM());  // do we get average and compare end result avg, or do we just monitor each independently
+  // eg: change to leftInital, rightInital. Distance travelled = average(DeltaLeft, DeltaRight) ?
+  backwardDrive(speed);
 
-while(distanceCM >= currentDistanceTravelled){
-currentDistanceTravelled = abs(average(leftEncoder.getDistanceCM(), rightEncoder.getDistanceCM()) - initialDistance);
+  while (distanceCM >= currentDistanceTravelled) {
+    currentDistanceTravelled = abs(average(leftEncoder->getDistanceCM(), rightEncoder->getDistanceCM()) - initialDistance);
+  }
+  currentDistanceTravelled = 0;
+  brake();  // will stop currently for testing, but this can be removed later to optimize
 }
-currentDistanceTravelled = 0;
-brake(); // will stop currently for testing, but this can be removed later to optimize
-}
 
 
-float Robot::average(float inputA, float inputB){
+float Robot::average(float inputA, float inputB) {
 
-  return (inputA+inputB)/2;
-
+  return (inputA + inputB) / 2;
 }
 // no longer used
 // //idle
